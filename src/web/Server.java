a0ad49fbea51cb30268eeb45460e395a6bb51970
Kirillp1988 <kirillp1988@gmail.com>
2022-06-30 15:58:54 +0300

    HttpRequest Class

--- src/web/Server.java
+++ src/web/Server.java
@@ -19,6 +19,7 @@ public class Server {
 
     public static final String CONTENT_TYPE_TEXT_HTML_CHARSET_UTF_8 = "Content-Type: text/html, charset=utf-8\n";
     public static final String HTTP_200_OK = "HTTP/1.1 200 OK\n";
+    public static final String EUR_BR = " EUR<br>";
 
     public static void main(String[] args) throws InvalidPathException {
         runServer();
@@ -145,14 +146,14 @@ public class Server {
         output.write(HTTP_200_OK);
         output.write(CONTENT_TYPE_TEXT_HTML_CHARSET_UTF_8);
         output.write("\n");
-        output.write("Total costs for Employer = " + calculationResponse.getTotalCostForEmployer() + " EUR<br>");
-        output.write("Social Tax = " + calculationResponse.getSocialTax() + " EUR<br>");
-        output.write("Unemployment Insurance Tax for Employer = " + calculationResponse.getUnemploymentInsuranceEmployer() + " EUR<br>");
-        output.write("Gross Salary = " + calculationResponse.getGrossSalary() + " EUR<br>");
-        output.write("II Funded Pension = " + calculationResponse.getFundedPension() + " EUR<br>");
-        output.write("Unemployment Insurance Tax for Employee = " + calculationResponse.getUnEmploymentInsuranceEmployee() + " EUR<br>");
-        output.write("Income Tax = " + calculationResponse.getIncomeTax() + " EUR<br>");
-        output.write("Net Salary = " + calculationResponse.getNetSalary() + " EUR<br>");
+        output.write("Total costs for Employer = " + calculationResponse.getTotalCostForEmployer() + EUR_BR);
+        output.write("Social Tax = " + calculationResponse.getSocialTax() + EUR_BR);
+        output.write("Unemployment Insurance Tax for Employer = " + calculationResponse.getUnemploymentInsuranceEmployer() + EUR_BR);
+        output.write("Gross Salary = " + calculationResponse.getGrossSalary() + EUR_BR);
+        output.write("II Funded Pension = " + calculationResponse.getFundedPension() + EUR_BR);
+        output.write("Unemployment Insurance Tax for Employee = " + calculationResponse.getUnEmploymentInsuranceEmployee() + EUR_BR);
+        output.write("Income Tax = " + calculationResponse.getIncomeTax() + EUR_BR);
+        output.write("Net Salary = " + calculationResponse.getNetSalary() + EUR_BR);
     }
 
     private static Path urlNotFound(BufferedWriter output, String pathString) throws IOException {
