9cf6fb747c3afc35cb054371c175ea7a7cb6b34b
Kirillp1988 <kirillp1988@gmail.com>
2022-07-08 20:08:10 +0300

    Basic auth. works

--- src/web/SocketService.java
+++ src/web/SocketService.java
@@ -128,9 +128,6 @@ public class SocketService {
                     }
                 }
             }
-//            else {
-//                errorUnauthorized(clientSocket);
-//            }
         }
     }
 
